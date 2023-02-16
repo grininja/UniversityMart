799db75ecf404a804123f5918ea3ca5ab2200fd6
grininja <raishubham675@gmail.com>
2023-02-17 00:07:47 +0530

    Added cart methods

---
+++ pages/api/adminOneRequests/cartHandler/getAllItem.js
@@ -0,0 +1,33 @@
+import dbConnect from "@/lib/mongoDb";
+import AdminOne from "@/models/adminOne";
+import Item from "@/models/item";
+const handler = async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      await dbConnect();
+      const { AdminOneId } = req.query;
+      const getCartItems = await AdminOne.findById({ _id: AdminOneId });
+      let cartItems = [];
+      if (getCartItems !== null) {
+        cartItems = getCartItems.cart;
+      }
+      let cartItemWithd = [];
+      for (let i = 0; i < cartItems.length; i++) {
+        let product = cartItems[i].product;
+        let description = await Item.findById({ _id: product });
+        cartItemWithd.push({
+          detail: description,
+          quantity: cartItems[i].quantity,
+        });
+      }
+      return res.status(200).send({ message: cartItemWithd });
+    } else {
+      res.status(400).send({ message: "Bad request" });
+    }
+  } catch (e) {
+    console.log("exception occured in getting cart items", e);
+    return res.status(404).send({ message: "error getting cart items" });
+  }
+};
+
+export default handler;
--- pages/api/adminOneRequests/cartHandler/reduceQuantity.js
+++ pages/api/adminOneRequests/cartHandler/reduceQuantity.js
@@ -1,10 +1,46 @@
 import dbConnect from "@/lib/mongoDb";
 import AdminOne from "@/models/adminOne";
-import mongoose from "mongoose";
 
+const handler = async (req, res) => {
+  try {
+    if (req.method === "POST") {
+      await dbConnect();
+      const { ItemId, departmentId, adminOneId } = req.body;
+      const checkIfAlreadyInCart = await AdminOne.find({
+        _id: adminOneId,
+        department: departmentId,
+        cart: { $elemMatch: { product: ItemId } },
+      });
+      if (checkIfAlreadyInCart !== null && checkIfAlreadyInCart.length > 0) {
+        if (checkIfAlreadyInCart[0].cart[0].quantity === 1) {
+          return res.status(200).send({ message: "Can not reduce quantity" });
+        }
+        await AdminOne.updateOne(
+          {
+            _id: adminOneId,
+            department: departmentId,
+            "cart.product": ItemId,
+          },
+          {
+            $set: {
+              "cart.$.quantity": Math.max(
+                1,
+                checkIfAlreadyInCart[0].cart[0].quantity - 1
+              ),
+            },
+          }
+        );
+      }
+      return res.status(200).send({ message: "Item quantity updated" });
+    } else {
+      return res.status(400).send({ message: "Invalid request" });
+    }
+  } catch (e) {
+    console.log("some exception occured while reducing quantiyt", e);
+    return res
+      .status(404)
+      .send({ message: "some exception occured while reducing quantity" });
+  }
+};
 
-const handler=(req,res)=>{
-
-}
-
-export default handler
+export default handler;
